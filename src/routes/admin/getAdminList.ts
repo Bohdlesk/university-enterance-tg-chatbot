fda3bde746e19a12e2b8e88b7a648a33576eb0b4
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-16 12:41:52 +0200

    Added exception handling

--- src/routes/admin/getAdminList.ts
+++ src/routes/admin/getAdminList.ts
@@ -1,21 +1,25 @@
 import express from 'express';
 import { Op } from 'sequelize';
 import { User } from '../../models';
+import { UserTypes } from '../../models/UserType';
 
 const adminsListRouter = express.Router();
 
 adminsListRouter.get('/', async (req, res) => {
   try {
-    const users = await User.findAll({
+    const admins = await User.findAll({
       where: {
         type_id: {
           [Op.or]: [UserTypes.admin, UserTypes.superAdmin],
         },
       },
     });
+    if (admins.length === 0) {
+      throw new Error('Admin users not found');
+    }
     res.status(200).json({
       status: 'success',
-      users,
+      users: admins,
     });
   } catch (error) {
     res.status(500).json({
