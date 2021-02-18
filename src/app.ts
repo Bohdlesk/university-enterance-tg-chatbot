d590de50d9011eb9615b712ee88804068612a442
Bohdlesk <bohdanleskovets@gmail.com>
2021-02-18 17:31:38 +0200

    google cred fix

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import fs from "fs"
 
 import { client } from './redisClient';
 
 import { connectToDB } from './bd';
 import { APIV1Router } from './routes';
 import { validatorsMiddlewares, loggerMiddlewares } from './middlewares';
+import {googleCredentials} from "./const";
 
 const app = express();
 
@@ -25,6 +27,13 @@ app.use(loggerMiddlewares.responseLogger);
 app.use('/api/v1', APIV1Router);
 
 app.use(loggerMiddlewares.errorLogger);
+
+fs.writeFile("google-credentials.json", JSON.stringify(googleCredentials), function(error: any){
+    if(error) throw error;
+    console.log("Асинхронная запись файла завершена. Содержимое файла:");
+    let data = fs.readFileSync("google-credentials.json", "utf8");
+    console.log(data);  // выводим считанные данные
+});
 
 // connect to database
 connectToDB();
--- src/const.ts
+++ src/const.ts
@@ -7,11 +7,13 @@ declare let process: {
         DATABASE_URL: string,
         REDIS_URL: string,
         DIALOGFLOW_PROJECT_ID: string,
+        GOOGLE_CREDENTIALS: object
     }
 };
 
 const conString = process.env.DATABASE_URL;
 const redisUrl = process.env.REDIS_URL;
 const dialogflowProjectId = process.env.DIALOGFLOW_PROJECT_ID;
+const googleCredentials = process.env.GOOGLE_CREDENTIALS;
 
-export { conString, redisUrl, dialogflowProjectId };
+export { conString, redisUrl, dialogflowProjectId, googleCredentials };
