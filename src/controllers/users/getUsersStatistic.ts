535f94888019f2537cfb610594296e81f5b5ef46
Buzivskoy Vyacheslav <v.buzivskoy@gmail.com>
2021-02-19 11:52:10 +0200

    fix user statistic route and remove caching

--- src/controllers/users/getUsersStatistic.ts
+++ src/controllers/users/getUsersStatistic.ts
@@ -1,29 +1,11 @@
 import { Request, Response } from 'express';
-import { promisify } from 'util';
+
 import { User } from '../../models';
-import { client } from '../../redisClient';
 
 export default async (req: Request, res: Response): Promise<Response> => {
   try {
     const queryParams = { ...req.query };
-
-    const getAsync = promisify(client.get).bind(client);
-
-    const usersAmount = await getAsync('usersAmount');
-    console.log(usersAmount);
-
-    if (usersAmount) {
-      return res.status(200).json({
-        status: 'success',
-        msg: 'cache',
-        users: usersAmount,
-      });
-    }
-
     const amount = await User.count({ where: queryParams });
-
-    client.set('usersAmount', `${amount}`);
-    client.expire('usersAmount', 20);
 
     return res.status(200).json({
       status: 'success',
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,11 +11,11 @@ import {
 
 const usersRouter = Router();
 
+usersRouter.get('/statistics', getUsersAmountController);
 usersRouter.post('/', createUserController);
 usersRouter.delete('/', deleteUserController);
 usersRouter.get('/', getAllUsersController);
 usersRouter.get('/:id', getUserByIdController);
 usersRouter.put('/', updateUserDataController);
-usersRouter.get('/statistics', getUsersAmountController);
 
 export { usersRouter };
