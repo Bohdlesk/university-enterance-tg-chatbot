1fb3bfe15917093348c5a2fcdd16f60dfdb92109
Bohdlesk <bohdanleskovets@gmail.com>
2021-05-05 17:52:41 +0300

    test

--- migrations/migration-all.js
+++ migrations/migration-all.js
@@ -1,129 +1,16 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('user_types', {
-      name: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        allowNull: false,
-      },
+  up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => queryInterface.addColumn(
+    'users',
+    'email',
+    {
+      type: Sequelize.STRING,
     },
     {
-      timestamps: false,
-    });
-    await queryInterface.createTable('user_roles', {
-      name: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        allowNull: false,
-      },
+      transaction: t,
     },
-    {
-      timestamps: false,
-    });
-    await queryInterface.createTable('bot_settings', {
-      name: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-      },
-      value: {
-        type: Sequelize.STRING(2000),
-      },
-    },
-    {
-      timestamps: false,
-    });
-    await queryInterface.createTable('faqs', {
-      name: {
-        type: Sequelize.STRING,
-        primaryKey: true,
-        allowNull: false,
-      },
-      question: {
-        type: Sequelize.STRING(2000),
-        allowNull: false,
-      },
-      answer: {
-        type: Sequelize.STRING(2000),
-        allowNull: false,
-      },
-      stats: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-      },
-    });
-    await queryInterface.createTable('unanswered_questions', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
-      },
-      question: {
-        type: Sequelize.STRING(2000),
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        defaultValue: Date.now(),
-      },
-    },
-    {
-      timestamps: false,
-    });
-    await queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        allowNull: false,
-      },
-      username: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      name: {
-        type: Sequelize.STRING,
-      },
-      phone_number: {
-        type: Sequelize.STRING,
-        unique: true,
-      },
-      type_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: 'Applicant',
-        references: {
-          model: { tableName: 'user_types' },
-          key: 'name',
-        },
-      },
-      role_name: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        defaultValue: 'regular',
-        references: {
-          model: { tableName: 'user_roles' },
-          key: 'name',
-        },
-      },
-      city: {
-        type: Sequelize.STRING,
-      },
-      state: {
-        type: Sequelize.JSON,
-        allowNull: false,
-      },
-    },
-    {
-      timestamps: false,
-    });
-  },
+  )),
 
-  down: async (queryInterface) => {
-    await queryInterface.dropTable('unanswered_questions');
-    await queryInterface.dropTable('users');
-    await queryInterface.dropTable('user_roles');
-    await queryInterface.dropTable('user_types');
-    await queryInterface.dropTable('faqs');
-    await queryInterface.dropTable('bot_settings');
-  },
+  down: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction((t) => queryInterface.removeColumn('users', 'email', {
+    transaction: t,
+  })),
 };
